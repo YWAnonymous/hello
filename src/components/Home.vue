4e928aa2f9affced240c516cfd99d0e291a76878
YWAnonymous <1274315645@qq.com>
2020-08-25 10:06:49 +0800

    左边菜单

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,20 +5,30 @@
       <el-container>
         <el-aside>
           <!--左侧菜单 -->
-          <el-menu>
-            <el-submenu index="1">
+          <el-menu router :default-active="activePath">
+            <el-submenu
+              v-for="menu in menus"
+              :key="menu.id"
+              :index="menu.id + ''"
+            >
               <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>导航一</span>
+                <span>{{ menu.authName }}</span>
               </template>
-              <el-menu-item-group>
-                <template slot="title">分组一</template>
-              </el-menu-item-group>
+              <el-menu-item
+                :index="'/' + children.path"
+                v-for="children in menu.children"
+                :key="children.id"
+                @click="saveNavState('/' + subItem.path)"
+              >
+                <template slot="title">{{ children.authName }}</template>
+              </el-menu-item>
             </el-submenu>
           </el-menu>
         </el-aside>
         <el-container>
-          <el-main>Main</el-main>
+          <el-main>
+            <router-view></router-view>
+          </el-main>
           <el-footer>Footer</el-footer>
         </el-container>
       </el-container>
@@ -30,14 +40,25 @@
 export default {
   name: "Home",
   data() {
-    return {};
+    return {
+      menus: "",
+      // 被激活导航地址
+      activePath: ""
+    };
   },
-  methods: {},
+  methods: {
+    // 保存连接的激活地址
+    saveNavState(activePath) {
+      window.sessionStorage.setItem("activePath", activePath);
+    }
+  },
   created() {
-    let _this= this;
-    this.$http.get("menus").then(function(response){
-      console.log(response);
+    let _this = this;
+    this.$http.get("menus").then(function(response) {
+      console.log(response.data.data);
+      _this.menus = response.data.data;
     });
+    this.activePath = window.sessionStorage.getItem("activePath");
   }
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,52 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Home from '@/components/Home'
+import VueRouter from 'vue-router'
+import HelloWorld from '../components/HelloWorld'
+import Home from '../components/Home'
+import Users from '../components/User'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
 const routes = [
-  
-  { path: '/', redirect: '/HelloWorld' },{
+
+  {
+    path: '/',
+    redirect: '/HelloWorld'
+  },
+  {
     path: '/HelloWorld',
     name: 'HelloWorld',
     component: HelloWorld
   },
   {
     path: '/home',
-    component: Home
+    name: 'Home',
+    component: Home,
+    children:[
+      {path: '/users',component:Users}
+    ]
   }
 ];
-const router = new Router({
+const router = new VueRouter({
   routes
 })
 
 router.beforeEach((to, from, next) => {
-  if (to.name == '/HelloWorld') return next()
-  // 获取token
-  const tokenStr = window.sessionStorage.getItem('token')
-  if(!tokenStr) return next('/login')
-  next()
+  // if (to.name == '/HelloWorld') return next()
+  //  获取token
+  // const tokenStr = window.sessionStorage.getItem('token')
+  // if(!tokenStr) return next('/HelloWorld')
+  // next()
+  const tokenStr = window.sessionStorage.getItem('token');
+  if (to.path != '/HelloWorld' && !tokenStr) {
+    next({
+      path: '/login'
+    })
+  } else {
+    if (to.path == '/login' && tokenStr) {
+      next('/home')
+    } else {
+      next()
+    }
+  }
 })
 export default router;
